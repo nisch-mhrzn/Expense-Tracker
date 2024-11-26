279fd3995a037382ae469f1dabd4ffeac60c1468
Nischal <nischal.maharjan1233@gmail.com>
2024-11-26 21:21:49 +0545

    Css and App file updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 
 import './App.css';
+import {Header} from './components/Header';
+import {Balance} from './components/Balance';
+import { IncomeExpenses } from './components/IncomeExpenses';
+import { TransactionList } from './components/TransactionList'; 
+import { AddTransaction } from './components/AddTransaction';
+import { GlobalProvider } from './context/GlobalState';
+
 
 function App() {
   return (
-    <div>
-      Herllo world
+    <GlobalProvider>
+    <Header/>
+    <div className="container">
+      <Balance/>
+      <IncomeExpenses/>
+      <TransactionList/>
+      <AddTransaction/>
     </div>
+    </GlobalProvider>
   );
 }
 
